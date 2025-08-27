f2a6cea4f9c3f23b5498fb0fbc45cc9943cb0c0c
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-08-27 12:57:12 +0200

    done done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,47 +1,50 @@
 // write cool JS hwere!!
-    const userSelect = document.getElementById("userSelect");
-    const nameInput = document.getElementById("nameInput");
-    const ageInput = document.getElementById("ageInput");
-    const colorInput = document.getElementById("colorInput");
-    const saveButton = document.getElementById("saveButton");
-    
-    // Event listener: vis data for valgt bruger
-  function userSelectChange() {
-      const selectedUser = userSelect.value;
-       console.log('change user to: '+selectedUser);
-      // TODO: Hent data fra localStorage og vis i formularen
-      const userData = JSON.parse(localStorage.getItem(selectedUser));
-      if (userData) {
-          nameInput.value = userData.name || "";
-          ageInput.value = userData.age || "";
-          colorInput.value = userData.color || "";
-      }
-    };
+const userSelect = document.getElementById("userSelect");
+const nameInput = document.getElementById("nameInput");
+const ageInput = document.getElementById("ageInput");
+const colorInput = document.getElementById("colorInput");
+const saveButton = document.getElementById("saveButton");
 
-    // Event listener: gem data fra formularen
-    saveButton.addEventListener("click", () => {
-        console.log("save data");
-        
-      const selectedUser = userSelect.value;
-      const name = nameInput.value;
-      const age = ageInput.value;
-      const color = colorInput.value;
+// Event listener: vis data for valgt bruger
+function userSelectChange() {
+    const selectedUser = userSelect.value;
+    console.log('change user to: ' + selectedUser);
 
-      // TODO: Gem data i localStorage for den valgte bruger
-      const userData = {
-          name: name,
-          age: age,
-          color: color
-      };
-      localStorage.setItem(selectedUser, JSON.stringify(userData));
-    });
+    // TODO: Hent data fra localStorage og vis i formularen
+    let users = JSON.parse(localStorage.getItem("users")) || {};
 
-    // Vis data for første bruger ved load
-    window.onload = userSelectChange;
-    userSelect.addEventListener("change", userSelectChange);
-    
-    
-    // --- change background color on save -----
-    // saveButton.addEventListener("click", () => {
-    //     document.body.style.backgroundColor = colorInput.value;
-    // });
+    // hvis brugeren ikke findes endnu, lav tomt objekt
+    if (!users[selectedUser]) {
+        users[selectedUser] = { name: "", age: "", color: "" };
+        localStorage.setItem("users", JSON.stringify(users));
+    }
+
+    const userData = users[selectedUser];
+    nameInput.value = userData.name || "";
+    ageInput.value = userData.age || "";
+    colorInput.value = userData.color || "";
+};
+
+// Event listener: gem data fra formularen
+saveButton.addEventListener("click", () => {
+    console.log("save data");
+
+    const selectedUser = userSelect.value;
+    const name = nameInput.value;
+    const age = ageInput.value;
+    const color = colorInput.value;
+
+    // TODO: Gem data i localStorage for den valgte bruger
+    let users = JSON.parse(localStorage.getItem("users")) || {};
+    users[selectedUser] = { name, age, color };
+    localStorage.setItem("users", JSON.stringify(users));
+});
+
+// Vis data for første bruger ved load
+window.onload = userSelectChange;
+userSelect.addEventListener("change", userSelectChange);
+
+// --- change background color on save -----
+saveButton.addEventListener("click", () => {
+    document.body.style.backgroundColor = colorInput.value;
+});
